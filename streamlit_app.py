d4b73fd963730bb63efe94b03bd96a03ae941777
Matias Gratz <73258767+matiasgratz@users.noreply.github.com>
2024-09-02 11:06:47 -0300

    Update streamlit_app.py
    Rollback al principio

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,25 +2,9 @@
 import streamlit as st
 from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
-import streamlit as st
-from snowflake.snowpark import Session
-from snowflake.snowpark.functions import udf
 
-# Create a single Snowpark session
-session = Session.builder.configs({
-    "account": "VEICLSU-PC92155",
-    "user": "MGRATZ2",
-    "password": "6h#oiZgigEy2u3",
-    "warehouse": "COMPUTE_WH",
-    "database": "SMOOTHIES",
-    "schema": "PUBLIC"
-}).create()
-
-# Define and register a UDF
-@udf(session=session, return_type=IntegerType())
-def my_udf(x: int) -> int:
-    return x * 2
-
+cnx = st.connection("snowflake")
+session = cnx.session()
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize your smoothie :cup_with_straw:")
@@ -28,7 +12,7 @@ st.title(":cup_with_straw: Customize your smoothie :cup_with_straw:")
 name_on_order = st.text_input("Name on Smoothie")
 st.write("The name on your smoothie will be", name_on_order)
 
-# session = get_active_session()
+#session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 ingredients_list =  st.multiselect(
