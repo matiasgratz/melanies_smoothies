cc0e37f90ef6328c70cf7068a232c94dba978075
Matias Gratz <73258767+matiasgratz@users.noreply.github.com>
2024-09-02 10:47:37 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,10 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark import Session
-from snowflake.snowpark.functions import col, udf
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
 
-# Create a single Snowpark session
-session = Session.builder.configs({
-    "account": "my_account",
-    "user": "MGRATZ2",
-    "password": "6h#oiZgigEy2u3",
-    "warehouse": "COMPUTE_WH",
-    "database": "SMOOTHIES",
-    "schema": "PUBLIC"
-}).create()
-
-# Define and register a UDF
-@udf(session=session)
-def my_udf(x):
-    return x * 2
+cnx = st.connection("snowflake")
+session = cnx.session()
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize your smoothie :cup_with_straw:")
